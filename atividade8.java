010628165f37c9f541ee669142847a1be78d4f1b
ArthurGmartins2307 <arthurgmgalvao@gmail.com>
2026-07-23 22:25:42 -0300

    Atividade oito

---
+++ atividade8.java
@@ -0,0 +1,34 @@
+public class atividade8 {
+    public static void main(String[] args) {
+        Carro carroArthur = new Carro("Jeep", "Renegade");
+        carroArthur.frear();
+        carroArthur.mostrarVelocidade();
+    }
+}
+
+class Carro{
+    String marca;
+    String modelo;
+    int velocidade;
+
+    public Carro(String marca, String modelo){
+        this.marca = marca;
+        this.modelo = modelo;
+        velocidade = 0;
+    }
+
+    public void acelerar(){
+        this.velocidade += 10;
+    }
+    public void frear(){
+        if (velocidade >= 10) {
+            velocidade -= 10;
+        }else{
+            System.out.println("O carro já está parado.");
+        }
+    }
+
+    public void mostrarVelocidade(){
+        System.out.println("Velocidade: " + velocidade + "km/h");
+    }
+}
